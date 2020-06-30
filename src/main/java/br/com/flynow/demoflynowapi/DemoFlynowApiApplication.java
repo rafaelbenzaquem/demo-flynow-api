953d1c53a2cdd310e1025b733690fbed640d338b
Rafael Benzaquem Neto <rafael.neto@outlook.com>
2020-06-30 20:43:32 -0300

    Projeto criado

---
+++ src/main/java/br/com/flynow/demoflynowapi/DemoFlynowApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.flynow.demoflynowapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFlynowApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoFlynowApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/flynow/demoflynowapi/DemoFlynowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.flynow.demoflynowapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFlynowApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
